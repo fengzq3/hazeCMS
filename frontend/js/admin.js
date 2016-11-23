ea1389b53a9de50f63d935c5820e5c8c1bcc4741
fengpc <zhiqiang.feng@qq.com>
2016-11-23 17:46:50 +0800

    侧边菜单ok

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -3,15 +3,21 @@
  */
 $(function () {
     $("[data-toggle='tooltip']").tooltip();
+    //定义变量
     const $minMenu = $('.js-mainMenu');
+    const $menuSlide = $('.js-menuSlide');
 
+    //菜单min方法
     $minMenu.on('click',function () {
-        let parentMenu = $(this).parent().parent();
-        if(!parentMenu.hasClass('min-menu')){
-            parentMenu.addClass('min-menu');
-        }else{
-            parentMenu.removeClass('min-menu');
-        }
+        $(this).parent().parent().toggleClass('min-menu');
+        $menuSlide.find('b.iconfont').attr({title:$menuSlide.find('span').text()});
+    });
+
+    //伸缩
+    $menuSlide.on('click',function () {
+        $(this).toggleClass('on');
+        $(this).next('ul').toggleClass('hide');
+
     });
 
 });
--- server/public/javascripts/admin.bundle.js
+++ server/public/javascripts/admin.bundle.js
@@ -2,13 +2,28 @@ webpackJsonp([0],[
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	/* WEBPACK VAR INJECTION */(function($) {"use strict";
+	/* WEBPACK VAR INJECTION */(function($) {'use strict';
 
 	/**
 	 * Created by feng on 2016/11/21.
 	 */
 	$(function () {
-	  $("[data-toggle='tooltip']").tooltip();
+	    $("[data-toggle='tooltip']").tooltip();
+	    //定义变量
+	    var $minMenu = $('.js-mainMenu');
+	    var $menuSlide = $('.js-menuSlide');
+
+	    //菜单min方法
+	    $minMenu.on('click', function () {
+	        $(this).parent().parent().toggleClass('min-menu');
+	        $menuSlide.find('b.iconfont').attr({ title: $menuSlide.find('span').text() });
+	    });
+
+	    //伸缩
+	    $menuSlide.on('click', function () {
+	        $(this).toggleClass('on');
+	        $(this).next('ul').toggleClass('hide');
+	    });
 	});
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
 
