b21dd239eee590f5f279feb2d4c5616397318eef
fengpc <zhiqiang.feng@qq.com>
2016-12-19 18:20:50 +0800

    常规提交

--- server/control/article.server.control.js
+++ server/control/article.server.control.js
@@ -12,32 +12,51 @@ const article = {
          * 访问连接为：article/id
          * 读取id，查询详情
          */
-        let siteP = db.readSiteInfo();
-        let navP = db.getNav();
+
         let detail = db.artDetail({_id:req.params.id});
 
-        Promise.all([siteP,navP,detail]).then(function (d) {
-            const data = {
-                link: {
-                    baseUrl: req.baseUrl,
-                    path: req.path
-                },
-                user: req.session.user,
-                site: {
-                    site_name: d[0].site_name,
-                    site_title: d[2].title + '_' + d[0].site_name,
-                    site_link:d[0].site_link,
-                    site_description:d[2].body.substr(0,160),
-                    site_keyword:d[2].tags
-                },
-                nav: d[1],
-                content: d[2]
-            };
 
-            res.render('article',data);
+        detail.then(function (dt) {
+            //获取tag列表
+            let tgAr = dt.tags.split(',');
+            let articleAr = [];
+            articleAr.push(db.readSiteInfo());
+            articleAr.push(db.getNav());
+            //根据tag查询相关文章
+
+            //todo 这里还有点问题，需要准确获得tag数量
+            tgAr.each(function () {
+                articleAr.push(db.getTagArticle(this,3,0));
+            });
+
+            //最终渲染
+            Promise.all(articleAr).then(function (d) {
+                const data = {
+                    link: {
+                        baseUrl: req.baseUrl,
+                        path: req.path
+                    },
+                    user: req.session.user,
+                    site: {
+                        site_name: d[0].site_name,
+                        site_title: d[2].title + '_' + d[0].site_name,
+                        site_link:d[0].site_link,
+                        site_description:d[2].body.substr(0,160),
+                        site_keyword:d[2].tags
+                    },
+                    nav: d[1],
+                    content: dt
+                };
+
+                res.render('article',data);
+            });
+            //内premise END
+
         },function (e) {
             next(e);
         });
+
+
 
     },
     prevNext:function (req, res, next) {
