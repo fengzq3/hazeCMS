87b018e483768427bc068d96ef66a572e417bd7d
feng <zhiqiang.feng@qq.com>
2017-01-14 21:27:35 +0800

    启用https

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -276,7 +276,7 @@ $(function () {
         // const duoshuoQuery = {short_name:"fengzq"};
         let ds = document.createElement('script');
         ds.type = 'text/javascript';ds.async = true;
-        ds.src = 'http://static.duoshuo.com/embed.js';
+        ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
         ds.charset = 'UTF-8';
         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ds);
     }
